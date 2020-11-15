f50a5cdbf91b545d3cfc409c17eda5172dcd34b7
Moutella <joaogabrielmsc@gmail.com>
2020-11-15 17:25:09 -0300

    fixed rotations again, top was inverted

--- cube.pl
+++ cube.pl
@@ -1,17 +1,17 @@
 %Gira a cima, X->Y horário, X<-Y anti-horário
 %são equivalentes a girar o baixo anti-horário/horário
-cimaHorario([[Cima1,Cima2,Cima3,Cima4],
-            [Frente1,Frente2,Frente3,Frente4],
-            [Dir1,Dir2,Dir3,Dir4],
-            [Tras1,Tras2,Tras3,Tras4],
-            [Esq1,Esq2,Esq3,Esq4],
-            [Baixo1,Baixo2,Baixo3,Baixo4]],
-            
-            [[Cima4,Cima1,Cima3,Cima2],
+cimaHorario([[Cima3,Cima1,Cima4,Cima2],
             [Esq1,Esq2,Frente3,Frente4],
             [Frente1,Frente2,Dir3,Dir4],
             [Dir1,Dir2,Tras3,Tras4],
             [Tras1,Tras2,Esq3,Esq4],
+            [Baixo1,Baixo2,Baixo3,Baixo4]],
+            
+            [[Cima1,Cima2,Cima3,Cima4],
+            [Frente1,Frente2,Frente3,Frente4],
+            [Dir1,Dir2,Dir3,Dir4],
+            [Tras1,Tras2,Tras3,Tras4],
+            [Esq1,Esq2,Esq3,Esq4],
             [Baixo1,Baixo2,Baixo3,Baixo4]]
             ).
 
@@ -27,7 +27,7 @@ direitaHorario([[Cima1,Cima2,Cima3,Cima4],
 
             [[Cima1,Frente2,Cima3,Frente4],
             [Frente1,Baixo2,Frente3,Baixo4],
-            [Dir4, Dir1,Dir3,Dir2],
+            [Dir3, Dir1,Dir4,Dir2],
             [Cima4,Tras2,Cima2,Tras4],
             [Esq1,Esq2,Esq3,Esq4],
             [Baixo1,Tras3,Baixo3,Tras1]]
@@ -43,7 +43,7 @@ frenteHorario([[Cima1,Cima2,Cima3,Cima4],
             [Baixo1,Baixo2,Baixo3,Baixo4]], 
               
             [[Cima1,Cima2,Esq4,Esq2],
-            [Frente4,Frente1,Frente3,Frente2],
+            [Frente3,Frente1,Frente4,Frente2],
             [Cima3, Dir2,Cima4,Dir4],
             [Tras1,Tras2,Tras3,Tras4],
             [Esq1,Baixo1,Esq3,Baixo2],
